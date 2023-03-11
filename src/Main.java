0d12777cbcaf973d96c10cec944ca4604418d3f3
Elena Zimnickaya <nazarova.leno4ka@yandex.ru>
2023-03-11 16:35:10 +0400

    CommitTwo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int priceTicket = 13676;
+        int mile = 20;
+
+        int bonus = priceTicket / mile;
+
+        System.out.println(" Вам начислено " + bonus + " бонусов ");
+
     }
 }
